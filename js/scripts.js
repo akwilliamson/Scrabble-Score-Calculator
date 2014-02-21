81a9e90c276fbcfc75ab03c53dae6cde2242c86d
epicodus <student@epicodus.com>
2014-02-21 15:03:04 -0800

    final edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,78 @@
-var scrabbleScore = function(userinput) {
-  var onePointLetters = ["a","e","i","o","u","l","n","r","s","t"];
-  for (var i = 0; i < onePointLetters.length; i++)
-    if (userinput.toLowerCase() === onePointLetters[i]) {
-    return 1;
-  }
-  var twoPointLetters = ["d","g"];
-  for (var i = 0; i < twoPointLetters.length; i++)
-    if (userinput.toLowerCase() === twoPointLetters[i]) {
-    return 2;
-  }
-  var threePointLetters = ["b","c","m","p"];
-  for (var i = 0; i < threePointLetters.length; i++)
-    if (userinput.toLowerCase() === threePointLetters[i]) {
-    return 3;
-  }
-  var fourPointLetters = ["f","h","v","w","y"];
-  for (var i = 0; i < fourPointLetters.length; i++)
-    if (userinput.toLowerCase() === fourPointLetters[i]) {
-    return 4;
-  }
-  var fivePointLetters = ["k"];
-  for (var i = 0; i < fivePointLetters.length; i++)
-    if (userinput.toLowerCase() === fivePointLetters[i]) {
-    return 5;
-  }
-  var eightPointLetters = ["j","x"];
-  for (var i = 0; i < eightPointLetters.length; i++)
-    if (userinput.toLowerCase() === eightPointLetters[i]) {
-    return 8;
-  }
-  var tenPointLetters = ["q","z"];
-  for (var i = 0; i < tenPointLetters.length; i++)
-    if (userinput.toLowerCase() === tenPointLetters[i]) {
-    return 10;
-  }
+var scrabbleScore = function(userWord) {
+  var score = 0;
+  var userWordArray = userWord.split("");
+  var alphabet = {'a': 1,'e': 1,'i': 1,'o': 1,'u': 1,'l': 1,'n': 1,'r': 1,'s': 1,'t': 1,'d': 2,'g': 2,'b': 3,'c': 3,'m': 3,'p': 3,'f': 4,'h': 4,'v': 4,'w': 4,'y': 4,'k': 5,'j': 8,'x': 8,'q': 10,'z': 10}
+  userWordArray.forEach(function(letter) {
+    score += alphabet[letter]
+  });
+return score;
 };
+
+$(document).ready(function() {
+  $("form#scrabble").submit(function(event) {
+    var word = ($("input#word").val());
+    var result = scrabbleScore(word);
+    $(".word").text(result);
+    $("#result").show();
+    event.preventDefault();
+  });
+});
+
+// var scrabbleScore = function(userinput) {
+//   var onePointLetters = ["a","e","i","o","u","l","n","r","s","t"];
+//   for (var i = 0; i < onePointLetters.length; i++)
+//     if (userinput.toLowerCase() === onePointLetters[i]) {
+//     return 1;
+//   }
+//   var twoPointLetters = ["d","g"];
+//   for (var i = 0; i < twoPointLetters.length; i++)
+//     if (userinput.toLowerCase() === twoPointLetters[i]) {
+//     return 2;
+//   }
+//   var threePointLetters = ["b","c","m","p"];
+//   for (var i = 0; i < threePointLetters.length; i++)
+//     if (userinput.toLowerCase() === threePointLetters[i]) {
+//     return 3;
+//   }
+//   var fourPointLetters = ["f","h","v","w","y"];
+//   for (var i = 0; i < fourPointLetters.length; i++)
+//     if (userinput.toLowerCase() === fourPointLetters[i]) {
+//     return 4;
+//   }
+//   var fivePointLetters = ["k"];
+//   for (var i = 0; i < fivePointLetters.length; i++)
+//     if (userinput.toLowerCase() === fivePointLetters[i]) {
+//     return 5;
+//   }
+//   var eightPointLetters = ["j","x"];
+//   for (var i = 0; i < eightPointLetters.length; i++)
+//     if (userinput.toLowerCase() === eightPointLetters[i]) {
+//     return 8;
+//   }
+//   var tenPointLetters = ["q","z"];
+//   for (var i = 0; i < tenPointLetters.length; i++)
+//     if (userinput.toLowerCase() === tenPointLetters[i]) {
+//     return 10;
+//   }
+// };
+
+// var wordScore = function(userinput) {
+//   var score = 0;
+//   var userInputArray = userinput.split("");
+//   for (var i = 0; i < userInputArray.length; i++) {
+//     score += scrabbleScore(userInputArray[i]);
+//   }
+// return score;        
+// };
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/specs.js
+++ spec/specs.js
@@ -1,25 +1,10 @@
 describe('scrabbleScore', function() {
-  it('returns 1 for an inputted letter that is valued at 1 point', function () {
-    scrabbleScore('e').should.equal(1);
-  });
-  it('returns 2 for an inputted letter that is valued at 2 points', function() {
-    scrabbleScore('g').should.equal(2);
-  });
-  it('returns 3 for an inputted letter that is valued at 3 points', function() {
-    scrabbleScore('b').should.equal(3);
-  });
-  it('returns 4 for an inputted letter that is valued at 4 points', function() {
-    scrabbleScore('f').should.equal(4);
-  });
-  it('returns 5 for an inputted letter that is valued at 5 points', function() {
-    scrabbleScore('k').should.equal(5);
-  });
-  it('returns 8 for an inputted letter that is valued at 8 points', function() {
-    scrabbleScore('j').should.equal(8);
-  });
-  it('returns 10 for an inputted letter that is valued at 10 points', function() {
-    scrabbleScore('q').should.equal(10);
+  it('returns the proper value for the first letter of the word', function () {
+    scrabbleScore('noun').should.equal(4);
   });
 });
+
+
+
 
 
